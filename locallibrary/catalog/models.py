396f677e978c74ee31799b4e259b939cd8043254
Ngô Huyền <ngohuyen@MacBook-Pro-2.local>
2021-08-03 10:53:03 +0700

    Django Tutorial Part 4: Django admin site

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -12,6 +12,7 @@ class Genre(models.Model):
         return self.name
 
 
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -27,6 +28,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
