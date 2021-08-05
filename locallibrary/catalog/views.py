c3b9062459b0377e0a5fc6dcbb18f7554d835431
Ngô Huyền <ngohuyen@MacBook-Pro-2.local>
2021-08-05 12:18:02 +0700

    Django Tutorial Part 7: Sessions framework

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -11,6 +11,10 @@ def index(request):
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(
         status__exact='a').count()
@@ -23,6 +27,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
